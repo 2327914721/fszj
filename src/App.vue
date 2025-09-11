d9e999a41e66513c9f435631c39fb4128489854a
fszj6666 <13247025+fszj6666@user.noreply.gitee.co1>
2025-09-11 17:51:29 +0800

    ss

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <GsapBasic />
-  <GsapScrollTrigger />
-</template>
-
-<script setup lang="ts">
-import GsapBasic from './components/GsapBasic.vue';
-import GsapScrollTrigger from './components/GsapScrollTrigger.vue';
-</script>
